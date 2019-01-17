7cae78315898571beffcf436bde7e357423ae155
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2019-01-17 20:02:21 +0000

    Add default stages to new users (#36)

--- WorkflowStreamer/src/main/java/com/workflowstreamer/manager/UsersManager.java
+++ WorkflowStreamer/src/main/java/com/workflowstreamer/manager/UsersManager.java
@@ -1,5 +1,6 @@
 package com.workflowstreamer.manager;
 
+import com.google.common.collect.ImmutableSet;
 import com.workflowstreamer.core.ImmutableLoginData;
 import com.workflowstreamer.core.ImmutableNewUser;
 import com.workflowstreamer.core.ImmutableUser;
@@ -11,6 +12,7 @@ import javax.ws.rs.core.Response;
 import java.util.Set;
 
 public class UsersManager {
+    private static final ImmutableSet<String> DEFAUL_STAGES = ImmutableSet.of("Back Log", "In Progress", "Done");
     private final UsersDAO usersDao;
 
     public UsersManager(UsersDAO usersDao) {
@@ -45,9 +47,18 @@ public class UsersManager {
             int userId = usersDao.insertUser(newUser.getEmail(), newUser.getUsername(), newUser.getPassword());
             ImmutableUser insertedUser = usersDao.getUserById(userId);
             response = Response.ok(insertedUser);
+            addDefaultStages(insertedUser.getUserId());
         } catch (UnableToExecuteStatementException e) {
             response = Response.status(Response.Status.FORBIDDEN);
         }
         return response.build();
+    }
+
+    private void addDefaultStages(int userId) {
+        try {
+            DEFAUL_STAGES.forEach(stage -> usersDao.insertUserStage(userId, stage));
+        } catch (UnableToExecuteStatementException e) {
+            e.printStackTrace();
+        }
     }
 }

e0ae8dbe22ce08844458a187dabb38b8ba16825e
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2019-04-09 04:54:46 +0100

    Fix CORS forevergit add .git add .

--- WorkflowStreamer/src/main/java/com/workflowstreamer/WorkflowStreamerApplication.java
+++ WorkflowStreamer/src/main/java/com/workflowstreamer/WorkflowStreamerApplication.java
@@ -17,7 +17,12 @@ import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import okhttp3.OkHttpClient;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.skife.jdbi.v2.DBI;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class WorkflowStreamerApplication extends Application<WorkflowStreamerConfiguration> {
     public static void main(final String[] args) throws Exception {
@@ -56,10 +61,23 @@ public class WorkflowStreamerApplication extends Application<WorkflowStreamerCon
         final ProjectsManager projectsManager = new ProjectsManager(projectsDAO, analyticsClient);
         final UsersManager usersManager = new UsersManager(usersDao, stagesDAO, analyticsClient, projectsManager, teamsManager);
 
-        // I could pass a manager here instead of a DAO
         environment.jersey().register(new TasksResource(tasksManager));
         environment.jersey().register(new UsersResource(usersManager));
         environment.jersey().register(new ProjectsResource(projectsManager));
         environment.jersey().register(new TeamsResource(teamsManager));
+        configureCors(environment);
+    }
+
+    private void configureCors(Environment environment) {
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 }
